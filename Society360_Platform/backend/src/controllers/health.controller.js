7404555fea6549274597ee0a3a4d60b2c82ffa35
Eeksha Holla R <111964966+EekshaHollaR@users.noreply.github.com>
2026-01-11 20:44:26 +0530

    removed the buggy backend folder and files

--- Society360_Platform/backend/src/controllers/health.controller.js
+++
@@ -1,8 +0,0 @@
-exports.healthCheck = (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Society360 Backend is running successfully",
-    company: "Civora Nexus Pvt. Ltd.",
-    project: "Society360 – Smart Residential Management System",
-  });
-};
